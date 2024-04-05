edb81447e93e27d6f44871caf42f7bf308379c32
ryandvl <contatoryandvl55@gmail.com>
2024-04-05 10:53:41 -0300

    Criação do Projeto

---
+++ src/main/java/rocketseat/com/nlwunitejava/NlwUniteJavaApplication.java
@@ -0,0 +1,13 @@
+package rocketseat.com.nlwunitejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwUniteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwUniteJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocketseat/com/nlwunitejava/NlwUniteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package rocketseat.com.nlwunitejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwUniteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
